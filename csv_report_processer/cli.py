561c99cae822c085edfce126d03b03a1ef872b89
myslak71 <Zabojca2golebi>
2019-01-30 20:51:00 +0100

    add CI

--- csv_report_processer/cli.py
+++ csv_report_processer/cli.py
@@ -1,6 +1,6 @@
 from argparse import ArgumentParser
 
-from csv_report_processor.csv_report_processer import CsvReportProcesser
+from csv_report_processer.csv_report_processer import CsvReportProcesser
 
 processer = CsvReportProcesser()
 
