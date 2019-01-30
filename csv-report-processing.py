ead76ff0c0416003ca6be333cbc41c66bca30f2f
myslak71 <Zabojca2golebi>
2019-01-30 12:03:29 +0100

    finish convert_state_to_country function

--- csv-report-processing.py
+++ csv-report-processing.py
@@ -2,21 +2,28 @@ import pandas
 import pycountry
 
 
-def convert_state_to_country():
-    pass
+def convert_state_to_country(state_name):
+    try:
+        state = pycountry.subdivisions.lookup(state_name)
+    except LookupError:
+        return 'XXX'
+    country = pycountry.countries.get(alpha_2=state.country_code)
+    return country.alpha_3
+
+
+print('wynik', convert_state_to_country('dupasad8'))
+
 
 def csv_report_processing():
     names = ['Date', 'Country', 'Number of impressions', 'CTR percentage']
 
     converters = {
         'Date': pandas.to_datetime,
-        'Country': convert_state_to_country
+        'Country': convert_state_to_country,
     }
 
-    input_report = pandas.read_csv('example_input_utf_8.csv', names=names, converters={'Date': pandas.to_datetime})
+    input_report = pandas.read_csv('example_input_utf_8.csv', names=names, converters=converters)
     print(input_report)
-    for row in input_report:
-        print(row)
 
 
 csv_report_processing()
