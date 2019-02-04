cba415f2da18cbfe4753f90c9b59870f0e2bca71
myslak71 <Zabojca2golebi>
2019-02-04 14:00:45 +0100

    change ArgumentParser description

--- csv_report_processer/cli.py
+++ csv_report_processer/cli.py
@@ -1,10 +1,32 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
 from csv_report_processer import ReportProcesser
 
+description = """Package reads from CSV file, with rows formatted as follows:
+mm/dd/yyy,state_name,number_of_impressions,CTR%
+and converts according to the table:
+_________________________________________________________________________________________________________
+|      INPUT FORMAT     |                               OUTPUT FORMAT                                   |
+|-----------------------|-------------------------------------------------------------------------------|
+|         date          |                              yyyy-mm-dd                                       |
+|       state_name      | three letter country code or 'XXX' if state_name does not exist in ISO 3166-2 |
+| number_of_impressions |                              casted to int                                    |
+|         CTR%          |              number of clicks based on CTR% and number of impressions         |
+---------------------------------------------------------------------------------------------------------
+
+In addition rows are aggregated and sorted lexicographically by date followed by the country code.
+
+If ERROR_PATH is specified, corrupted rows are excluded from the OUTPUT_PATH file and saved
+as CSV file.
+
+If ERROR_PATH is not specified, corrupted rows are not considered critical and are included in the OUTPUT_PATH
+file with appropriate error message.
+
+All program logs are stored in report_processer.log file, which is created in program run directory."""
+
 
 def get_parser():
-    parser = ArgumentParser(description='CSV Report Processing description')
+    parser = ArgumentParser(description=description, formatter_class=RawDescriptionHelpFormatter)
     required = parser.add_argument_group('required arguments')
     required.add_argument('-i', '--input', help='Input CSV file path', required=True)
     required.add_argument('-o', '--output', help='Output CSV file path', required=True)
@@ -16,7 +38,6 @@ def main():
     parser = get_parser().parse_args()
     processer = ReportProcesser()
     processer.process_csv_report(parser.input, parser.output, parser.errors)
-
 
 if __name__ == '__main__':
     main()
